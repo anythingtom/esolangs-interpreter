20826042b4f63458097132edaff09c2982a77769
anythingtom <71325886+anythingtom@users.noreply.github.com>
2021-02-07 18:58:50 +0700

    Compiler added. Check the compile() function to understand how it works.

--- My-esolangs/compiler.cpp
+++ My-esolangs/compiler.cpp
@@ -2,46 +2,57 @@
 
 using namespace std;
 
-void compile(string s){
-    long long arr[1001][1001];
+
+
+int main(){
+    string s;
+    int a[1001][1001] = {0};
+    cin >> s;
+    int arr[1001][1001];
+    for (int i = 0; i < 1001; i++){
+        for (int j = 0; i < 1001; i++){
+            arr[i][j] = 0;
+        }
+    }
     int ptx = 0;
     int pty = 0;
     int ptr = arr[ptx][pty];
     for (int i = 0; i < s.length(); i++){
         switch(s[i]){
                     case '^':
-                     pty++;
+                     pty++; //moves pointer 1 unit upwards
                      break;
                     case 'v':
-                     pty--;
+                     pty--; //moves pointer 1 unit downwards
                      break;
                     case '<':
-                     ptx--;
+                     ptx--; //moves pointer 1 unit leftwards
                      break;
                     case '>':
-                     ptx++;
+                     ptx++; //moves pointer 1 unit rightwards
                      break;
                     case '+':
-                     ptr++;
+                     ptr++; //increase cell value by 1
                      break;
                     case '-':
-                     ptr--;
+                     ptr--; //decrease cell value by 1
                      break;
                     case '=':
-                     cout << ptr << endl;
+                     cout << ptr << endl; //prints out cell value
                      break;
                     case '*':
-                     cout << "(" << ptx << ", " << pty << ")" << endl;
+                     cout << "(" << ptx << ", " << pty << ")" << endl; //prints out pointer position
                      break;
                     case '!':
-                     cout << (char)ptr << endl;
+                     cout << (char)ptr << endl; //prints out the cell calue as ASCII character
                      break;
                     case '#':
-                     ptr += 10;
+                     ptr += 10; //increase cell value by 10
                      break;
                     case '&':
-                     ptr -= 10;
+                     ptr -= 10; //decrease cell value by 10
                      break;
         }
     }
+    return 0;
 }
